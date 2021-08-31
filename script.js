9b6feb28cae2f2beb1218c09b3dfa4eb31118b5c
Samir Cunha <87715834+samircunha@users.noreply.github.com>
2021-08-31 19:20:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let play = 0;
 let jogadas = 0;
-let jogo = [];
+let jogo = ['', '', '', '', '', '', '', '', ''];
+let resultadosGanhador = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 let jogador1 = 'j1';
 let jogador2 = 'j2';
+let buttonRestart = document.querySelector('.btn');
 
 function jogada (casaJogada, jogada) {
     let casaClicada = document.querySelector(casaJogada);
@@ -12,14 +23,13 @@ function jogada (casaJogada, jogada) {
         jogo[jogada-1] = jogador1;
         jogadas++;
         play = 1;
-        aplicaImg(casaClicada, "<img src='./imagens/jogada1.png'>")
+        aplicaImg(casaClicada, "<img src='./imagens/casaStark.png'>")
         resultado(jogo, jogadas);
-;
     }else{
         jogo[jogada-1] = jogador2;
         jogadas++;
         play = 0;
-        aplicaImg(casaClicada, "<img src='./imagens/jogada2.png'>")
+        aplicaImg(casaClicada, "<img src='./imagens/casaLan.png'>")
         resultado(jogo, jogadas);
     }
     
@@ -31,37 +41,23 @@ function aplicaImg(posicao, img){
 
 function resultado(array, jogadas) {
     
-    if (array[0] == jogador1 && array[1] == jogador1 && array[2] == jogador1 ||
-        array[3] == jogador1 && array[4] == jogador1 && array[5] == jogador1 ||
-        array[6] == jogador1 && array[7] == jogador1 && array[8] == jogador1 ||
-        array[0] == jogador1 && array[3] == jogador1 && array[6] == jogador1 ||
-        array[1] == jogador1 && array[4] == jogador1 && array[7] == jogador1 ||
-        array[2] == jogador1 && array[5] == jogador1 && array[8] == jogador1 ||
-        array[0] == jogador1 && array[4] == jogador1 && array[8] == jogador1 ||
-        array[2] == jogador1 && array[4] == jogador1 && array[6] == jogador1){
-        setTimeout(() => {
-            alert('O jogo Acabou, O jogador 1 ganhou!');
-            window.location.reload();
-        }, 100) 
-    }else if (array[0] == jogador2 && array[1] == jogador2 && array[2] == jogador2 ||
-        array[3] == jogador2 && array[4] == jogador2 && array[5] == jogador2 ||
-        array[6] == jogador2 && array[7] == jogador2 && array[8] == jogador2 ||
-        array[0] == jogador2 && array[3] == jogador2 && array[6] == jogador2 ||
-        array[1] == jogador2 && array[4] == jogador2 && array[7] == jogador2 ||
-        array[2] == jogador2 && array[5] == jogador2 && array[8] == jogador2 ||
-        array[0] == jogador2 && array[4] == jogador2 && array[8] == jogador2 ||
-        array[2] == jogador2 && array[4] == jogador2 && array[6] == jogador2){
-        setTimeout(() => {
-            alert('O jogo Acabou, O jogador 2 ganhou!');
-            window.location.reload();
-        }, 100) 
-    }else if(jogadas == 9){
-        setTimeout(() => {
-            alert('O jogo Empatou, ninguém ganhou!');
-            window.location.reload();
-        }, 100) 
-    }
+    for (i of resultadosGanhador){
 
+        if (array[i[0]] == jogador1 && array[i[1]] == jogador1 && array[i[2]] == jogador1){
+            setTimeout(() => {
+                alert('O jogo Acabou, A Casa Stark ganhou!');
+            }, 100) 
+        }else if(array[i[0]] == jogador2 && array[i[1]] == jogador2 && array[i[2]] == jogador2){
+            setTimeout(() => {
+                alert('O jogo Acabou, A Casa Lannister ganhou!');
+            }, 100) 
+        }else if(jogadas === 9){
+            alert('O jogo empatou')
+        }
+    }
 }
 
 
+buttonRestart.addEventListener('click', function(){
+    window.location.reload();
+})
